ff6f62d7ee18c3fd49711c7ffffa48d66c84139d
ahmed-M32 <am.3443rrrr@gmail.com>
2024-11-07 06:06:16 +0200

    initial commit

---
+++ src/main/java/org/example/park_controller.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class park_controller {
+}
--- src/main/java/org/example/parking_lot.java
+++ src/main/java/org/example/parking_lot.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.util.concurrent.Semaphore;
+
 public class parking_lot {
+    public park<Thread>[];
+    public final Semaphore park_control;
+
+    parking_lot(){
+        park_control = new Semaphore(4);
+    }
 }
